5492052d21481d55a003d2be2d1e73564b39de0b
Christian Aichinger <Greek0@gmx.net>
2013-10-01 19:04:22 +0200

    Add favicon by embedding it in Javascript
    The icon is a purple background with white "Sl" letters. It is
    dynamically added via Javascript to avoid a separate roundtrip for the
    favicon. If Javascript is disabled, the favicon won't be present, but
    it's no big loss.

--- slasti/static_files/slasti.js
+++ slasti/static_files/slasti.js
@@ -1,5 +1,18 @@
 
 $(document).ready(function() {
+    var favIcon = "\
+    iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAM1BMVEWAF46iDrilHbawPcO1\
+    ScO5VcrAZc/CbtHIeNPNh9raqePitOTqzO/x3fT26Pb+8/r9//z/O9A1AAAAAXRSTlMAQObY\
+    ZgAAAAFiS0dEAIgFHUgAAABnSURBVBjTfc/BDsQgCARQB7Zoq9T5/6+tbtZN6aGc4AUSJiWE\
+    So95CJBb715A0r9QeJgVG/0PvK7tBU0iZLrJHbA1nuUOgFbWAMBOjWD8LJB9U83u/xM5x4v9\
+    0PnpqHkiKhKyvMdNF1yNBFxyrc1oAAAAAElFTkSuQmCC";
+
+    var docHead = document.getElementsByTagName('head')[0];
+    var newLink = document.createElement('link');
+    newLink.rel = 'shortcut icon';
+    newLink.href = 'data:image/png;base64,' + favIcon;
+    docHead.appendChild(newLink);
+
     $(".note").each(function(index, elem) {
         var converter = new Showdown.converter();
         var html = converter.makeHtml($(this).html());
