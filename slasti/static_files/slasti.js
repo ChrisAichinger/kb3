fce08620c52b5612c7f39e6c55afce013c235a1f
Christian Aichinger <Greek0@gmx.net>
2013-12-26 09:26:51 +0100

    Parse loc: data from notes and show it on a map
    Add a new tool to the top toolbar called "parse loc". It searches within
    bookmark notes for location data in the form "loc:48.210247,16.373488".
    Then it plots all the points of the current user on a map.
    
    This is rough-and-ready code. It could use improvement:
     * Find a better way to plot the map. Currently, we use
       copypastemap.com. It sucks, but has an easy to use interface.
     * Allow adding a further search string to filter the loc: results.
     * Implement about a useful UI to enter said filter string and to
       display the results in-line.
    
    Along the way, some unrelated changes were made:
     * Modify style.css, since the UI suddenly became messed up?!
       Maybe that was triggered by the move to HTML5 doctype earlier.
     * Add proper classes the generated HTML for bookmarks. This enables
       easy parsing of the bookmark data in javascript without needing extra
       json-export from the server code.

--- slasti/static_files/slasti.js
+++ slasti/static_files/slasti.js
@@ -13,9 +13,50 @@ $(document).ready(function() {
     newLink.href = 'data:image/png;base64,' + favIcon;
     docHead.appendChild(newLink);
 
+
     $(".note").each(function(index, elem) {
         var converter = new Showdown.converter();
         var html = converter.makeHtml($(this).html());
         $(this).html(html);
     });
+
+    $("#loc_parse").click(function(evt) {
+        // Load all loc: containing entries, then process them
+        var search_url = $("form[action *= 'search']").attr('action');
+        $.get(search_url + "?q=loc:&nopage=1", function(data) {
+            // Transform data into a jQuery object
+            var jq_data = $('<div/>').html(data);
+
+            // Iterate over bookmarks to find loc: directives and mark names
+            var results = [];
+            jq_data.find('.bookmark').each(function (i, elem) {
+                // Redeclare regex within loop. Otherwise we hit a FF bug:
+                // http://stackoverflow.com/questions/10167323
+                var loc_regex = /loc:([+-]?[.0-9]+),([+-]?[.0-9]+)/g;
+                var m = loc_regex.exec(elem.innerHTML);
+                if (!m) {
+                    return;
+                }
+                var lat = m[1];
+                var lon = m[2];
+                var name = $(elem).find('.mark_link').text();
+                results.push([lat.toString(), lon.toString(), "circle5", "red",
+                              "", name].join('\t'));
+            });
+
+            // Send the results to copypastemap.com
+            // See harrywood.co.uk/maps/examples/openlayers/marker-popups.html
+            // for a more robust solution.
+            results = results.join('\n');
+            var res = $('<form method="post" ' +
+              'action="http://copypastemap.com/map.php" style="display:none">' +
+              '<textarea name="xldata" id="xldata" style="width:100%">' +
+              results + '</textarea>' +
+              '<input name="Submit" id="Submit" value="MAP!" type="text" />' +
+              '</form>');
+            $("body").append(res);
+            res.submit();
+        });
+    });
+
 });
